1356f34b0a2a76f2110a6f5f6162b7c9b274392e
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2024-12-30 23:00:09 -0500

    Added: -> User level controller, service and repository. -> Implemented ExceptionAdvisor, and basic exception handling -> DTO's to convert objects to Dto before returning to client.

---
+++ src/main/java/com/waelsworld/userservice/Repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.waelsworld.userservice.Repositories;
+
+import com.waelsworld.userservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.net.InterfaceAddress;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
+}
