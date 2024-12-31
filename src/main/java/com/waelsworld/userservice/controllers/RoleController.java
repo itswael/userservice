e514dda41d07f5c248ea8c81c74b47cc3cbdbc33
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2024-12-31 12:20:31 -0500

    Added: -> Role controller, service and dto for creating a new role

---
+++ src/main/java/com/waelsworld/userservice/controllers/RoleController.java
@@ -0,0 +1,28 @@
+package com.waelsworld.userservice.controllers;
+
+import com.waelsworld.userservice.Dto.createRoleRequestDto;
+import com.waelsworld.userservice.models.Role;
+import com.waelsworld.userservice.services.RoleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+    RoleService roleService;
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    public ResponseEntity<Role> createRole(@RequestBody createRoleRequestDto request){
+        try{
+            Role role = roleService.createRole(request.getName());
+            return ResponseEntity.ok(role);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
